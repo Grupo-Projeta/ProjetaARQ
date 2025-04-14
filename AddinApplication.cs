60962d0a9449efed1864e61809e8fd8f51bcb463
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-04-14 11:38:26 -0300

    Inicilializando a base do Addin

--- AddinApplication.cs
+++ AddinApplication.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.UI;
 
 namespace ProjetaARQ
 {
-    public class AddinApplication
+    public class AddinApplication : IExternalApplication
     {
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+
+
+        public Result OnShutdown(UIControlledApplication application)
+            => Result.Succeeded;
+
     }
 }
