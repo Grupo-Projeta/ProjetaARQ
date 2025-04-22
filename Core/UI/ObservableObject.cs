dd86fff7e7be5292e7fe046bfc77b0313fba7af8
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-04-22 14:51:45 -0300

    Adicionado o Core para ViewModels

---
+++ Core/UI/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProjetaARQ.Core.UI
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
