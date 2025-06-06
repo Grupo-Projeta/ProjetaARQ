6309ccdac9df191f2721b3e9275e326afc6a4c73
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-06-06 14:15:50 -0300

    Download Multiplas Vezes Fixed

--- Features/FamiliesPanel/MVVM/FamilyItem.cs
+++ Features/FamiliesPanel/MVVM/FamilyItem.cs
@@ -10,13 +10,16 @@ namespace ProjetaARQ.Features.FamiliesPanel.MVVM
     internal class FamilyItem
     {
         public string Name { get; set; }
+        public string UpperName { get; set; }
+
         public string FilePath { get; set; }
         public string ThumbnailPath { get; set; }
 
         public FamilyItem(string filePath, string thumbsDirectory)
         {
             FilePath = filePath;
-            Name = Path.GetFileNameWithoutExtension(filePath).ToUpper();
+            Name = Path.GetFileNameWithoutExtension(filePath);
+            UpperName = Name.ToUpper();
 
             string thumbPath = Path.Combine(thumbsDirectory, Name + ".png");
             ThumbnailPath = File.Exists(thumbPath) ? thumbPath : Path.Combine(thumbsDirectory, "default.png");
