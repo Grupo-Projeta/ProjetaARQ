432fc1757cfbefde3477e8cbbd6b2ef82fec2cb7
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-06-06 15:44:51 -0300

    Added OpenXML

---
+++ Features/WordExport/Commands/WordExport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using ProjetaARQ.Features.Test.MVVM;
+using ProjetaARQ.Revit.Base;
+using ProjetaARQ.Revit.UI;
+
+namespace ProjetaARQ.Features.WordExport.Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class WordExport : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            
+
+            return Result.Succeeded;
+        }
+    }
+}
