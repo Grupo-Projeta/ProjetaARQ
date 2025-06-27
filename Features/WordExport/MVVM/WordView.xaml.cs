c17ee28c8a6da2042e735cb786ec7245abe4f603
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-06-27 17:34:26 -0300

    Criando interface - Criacao Presets

--- Features/WordExport/MVVM/WordView.xaml.cs
+++ Features/WordExport/MVVM/WordView.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MahApps.Metro.Controls;
+﻿using MahApps.Metro.Controls;
 
 namespace ProjetaARQ.Features.WordExport.MVVM
 {
@@ -21,9 +7,10 @@ namespace ProjetaARQ.Features.WordExport.MVVM
     /// </summary>
     public partial class WordView : MetroWindow
     {
-        public WordView()
+        internal WordView(WordViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
