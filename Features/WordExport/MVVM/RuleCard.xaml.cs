ca4d24023de2e429be1c95bb3601606dfae26562
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2025-07-01 01:32:50 -0300

    Apenas copiando errado

--- Features/WordExport/MVVM/RuleCard.xaml.cs
+++ Features/WordExport/MVVM/RuleCard.xaml.cs
@@ -1,23 +1,40 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
-namespace ProjetaARQ.Features.WordExport.MVVM // Garanta que o namespace esteja correto
+namespace ProjetaARQ.Features.WordExport.MVVM
 {
     public partial class RuleCard : UserControl
     {
         public RuleCard()
         {
             InitializeComponent();
-            this.Height = 120; // Altura inicial
+            // Altura inicial do cartão
+            this.Height = 150; 
+        }
+
+        // --- Lógica de Redimensionamento ---
+
+        private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            // Captura o mouse para garantir que o redimensionamento seja fluido
+            (sender as Thumb)?.CaptureMouse();
         }
 
         private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
+            // 'this' se refere à instância do RuleCard que está sendo arrastada
             double newHeight = this.ActualHeight + e.VerticalChange;
             if (newHeight >= this.MinHeight && newHeight <= this.MaxHeight)
             {
                 this.Height = newHeight;
             }
+        }
+
+        private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            // Libera o mouse ao final da operação
+            (sender as Thumb)?.ReleaseMouseCapture();
         }
     }
 }
